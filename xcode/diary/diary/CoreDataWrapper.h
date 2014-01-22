95260723d0145271597037f459bad67853be6e75
Carsten89 <muller@Mullers-Mac.local>
2014-01-22 19:39:37 +0100

    coreDataWrapper

---
+++ xcode/diary/diary/CoreDataWrapper.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataWrapper.h
+//  diary
+//
+//  Created by Müller on 22.01.14.
+//  Copyright (c) 2014 FMA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataWrapper : NSObject
+
+@end
---
+++ xcode/diary/diary/CoreDataWrapper.m
@@ -0,0 +1,29 @@
+//
+//  CoreDataWrapper.m
+//  diary
+//
+//  Created by Müller on 22.01.14.
+//  Copyright (c) 2014 FMA. All rights reserved.
+//
+
+#import "CoreDataWrapper.h"
+#import "Remote.h"
+#import "Entry.h"
+
+@implementation CoreDataWrapper
+
+-(id)init{
+
+    return self;
+}
+
+
+-(NSManagedObject*) getCoreDataObjFor:(NSDictionary *)dictionary{
+    
+    
+    
+    return nil;
+}
+
+
+@end
